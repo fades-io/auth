8195e947a94fccfef2edf66b6fe3f65118d48d42
Nikolay Shiryaev <deendeeps@list.ru>
2022-02-14 20:13:02 +0300

    фикс кастомной ошибки

---
+++ internal/apperror/error.go
@@ -0,0 +1,47 @@
+package apperror
+
+import "encoding/json"
+
+var (
+	ErrNotFound = New(nil, "Not found", "", "404")
+)
+
+// Кастомная ошибка, которая передается в json вместе с сообщением
+type AppError struct {
+	Err              error  `json:"-"` // исходная ошибка, поэтому в JSON она нам не нужна
+	Message          string `json:"message,omitempty"`
+	DeveloperMEssage string `json:"developer_message,omitempty"`
+	Status           string `json:"code,omitempty"`
+}
+
+// метод для соответствия интерфейсу Error{}
+func (appError *AppError) Error() string {
+	return appError.Message
+}
+
+func (appError *AppError) Unwrap() error {
+	return appError.Err
+}
+
+func (appError *AppError) Marshal() []byte {
+	marshal, err := json.Marshal(appError)
+	if err != nil {
+		return nil
+	}
+	return marshal
+}
+
+// Создает кастомную ошибку
+func New(err error, message, developerMessage, code string) *AppError {
+	return &AppError{
+		Err:              err,
+		Message:          message,
+		DeveloperMEssage: developerMessage,
+		Status:           code,
+	}
+}
+
+// Любую ошибку оборачиваем в системную
+func SystemError(err error) *AppError {
+	return New(err, "internal system error", err.Error(), "")
+}
--- internal/server/login_controller.go
+++ internal/server/login_controller.go
@@ -12,41 +12,39 @@ import (
 )
 
 // Обработка запрос для входа пользователя
-func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
+func (server *Server) Login(w http.ResponseWriter, r *http.Request) error {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatalf("Не удалось считать тело запроса: %v", err)
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		return
+		return err
 	}
 
 	user := domain.UserLogin{}
 	err = json.Unmarshal(body, &user)
 	if err != nil {
 		log.Fatalf("Не удалось смапить json в domain модель: %v", err)
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		return
+		return err
 	}
 
 	user.Prepare()
 	err = user.Validate("login")
 	if err != nil {
 		log.Fatalf("Не удалось смапить json в domain модель: %v", err)
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		return
+		return err
 	}
 	token, err := server.SignIn(user.Username, user.Password)
 	if err != nil {
 		log.Fatalf("Не удалось смапить json в domain модель: %v", err)
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		return
+		return err
 	}
 
 	w.WriteHeader(http.StatusOK)
 	err = json.NewEncoder(w).Encode(token)
 	if err != nil {
 		fmt.Fprintf(w, "%s", err.Error())
+		return err
 	}
+	return nil
 }
 
 func (server *Server) SignIn(username, password string) (string, error) {
