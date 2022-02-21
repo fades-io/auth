9d39a5ef5b7ed2bd602ec51d067def275aaac91d
Nikolay Shiryaev <deendeeps@list.ru>
2022-02-21 21:12:51 +0300

    фикс респонс токена

--- internal/domain/token.go
+++ internal/domain/token.go
@@ -4,8 +4,8 @@ package domain
 type Token struct {
 	ID      uint   `json:"-" gorm:"primary_key;auto_increment"`
 	Value   string `json:"token" gorm:"column:token"`
-	Created int64  `json:"created" gorm:"autoCreateTime:nano"`
-	Updated int64  `json:"updated" gorm:"autoUpdateTime:nano"`
-	Status  string `json:"token_status" gorm:"column:token_status"`
-	UserID  uint   `json:"user_id" gorm:"column:user_id"`
+	Created int64  `json:"created,omitempty" gorm:"autoCreateTime:nano"`
+	Updated int64  `json:"updated,omitempty" gorm:"autoUpdateTime:nano"`
+	Status  string `json:"token_status,omitempty" gorm:"column:token_status"`
+	UserID  uint   `json:"user_id,omitempty" gorm:"column:user_id"`
 }
